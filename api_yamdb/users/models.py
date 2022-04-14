5ef3f72296fbb3fa20b485c5c8503666b8f9f55b
Павел Пацей <patsey.pavel@gmail.com>
2022-04-14 21:20:19 +0300

    в usres.models поправил ROLE_CHOICES

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 ROLE_CHOICES = (
-    ("Пользователь", "user"),
-    ("Администратор", "admin"),
-    ("Модератор", "moderator"),
+    ("user", "user"),
+    ("admin", "admin"),
+    ("moderator", "moderator"),
 )
 
 
