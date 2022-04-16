be5b07b5f4f14c61a09a33d559fe135815c84e7f
Василий Рябов <ryabov.v.s@yandex.ru>
2022-04-16 18:25:20 +0300

    add lookup in viewset Category and Genre

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -49,6 +49,7 @@ class CategoryViewSet(CreateListDestroyViewSet):
     permission_classes = [IsAdminOrReadOnly]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    lookup_field = "slug"
     pagination_class = LimitOffsetPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ("name",)
@@ -69,6 +70,7 @@ class GenreViewSet(CreateListDestroyViewSet):
     permission_classes = [IsAdminOrReadOnly]
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    lookup_field = "slug"
     pagination_class = LimitOffsetPagination
     filter_backends = (filters.SearchFilter,)
     search_fields = ("name",)
