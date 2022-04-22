a3434f731566ff4d5d1dee1ad116095e56acd83e
Павел Пацей <patsey.pavel@gmail.com>
2022-04-22 15:52:03 +0300

    косметические изменения black and isort

--- api_yamdb/users/migrations/0002_auto_20220422_1250.py
+++ api_yamdb/users/migrations/0002_auto_20220422_1250.py
@@ -6,13 +6,20 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_initial'),
+        ("users", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='user',
-            name='role',
-            field=models.CharField(choices=[('user', 'user'), ('admin', 'admin'), ('moderator', 'moderator')], max_length=100),
+            model_name="user",
+            name="role",
+            field=models.CharField(
+                choices=[
+                    ("user", "user"),
+                    ("admin", "admin"),
+                    ("moderator", "moderator"),
+                ],
+                max_length=100,
+            ),
         ),
     ]
